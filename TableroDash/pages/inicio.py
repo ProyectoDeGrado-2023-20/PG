04013f549514f6ea13ce56f6bb73a1c91bb7ec64
Julicardeb <cardenasjulianacarolina@gmail.com>
2023-10-25 17:49:34 -0500

    dash

--- TableroDash/pages/inicio.py
+++ TableroDash/pages/inicio.py
@@ -2,18 +2,15 @@ from dash import html
 from apps import navigation
 import dash_bootstrap_components as dbc
 
-texto = '''Las enfermedades cardíacas son una de las principales causas de muerte en todo el mundo.
-Estas enfermedades afectan al corazón y al sistema circulatorio, lo que puede llevar a complicaciones
-graves, como ataques cardíacos y accidentes cerebrovasculares.
-
-El siguiente programa ha sido desarrollado como apoyo a médicos en el proceso de evaluación de
-pacientes para detectar si una persona tiene o no una enfermedad cardíaca y la toma de decisiones
-asociada como solicitud de exámenes, chequeos y otros procedimientos.
-
-Este programa se ha desarrollado mediante la exploración de datos y el modelado con
-redes bayesianas, con el fin de descubrir la relación entre 14 atributos relevantes en la base
-de datos de enfermedades cardíacas de Cleveland. Los datos se recopilaron del repositorio de la
-Universidad de California en Irvine.'''
+texto = '''El sector de salud y protección social es un ámbito complejo y vital para
+cualquier sociedad. Por esta razón, comprender los patrones de contratación que se
+llevan a cabo en este sector es fundamental para mejorar su funcionamiento y garantizar
+una atención adecuada a las personas que lo necesitan. Por medio de la investigación,
+se busca obtener un conocimiento profundo de las prácticas de contratación en el sector
+de salud y protección social, identificando las fortalezas y debilidades del sistema actual.
+Se busca presentar la herramienta realizada a personas expertas en el sector con el objetivo
+de tener una retroalimentación sobre los indicadores que se considerar y que tan acertada es
+la aplicación a partir de su experiencia.'''
 
 texto3 = 'Aplicación creada por Santiago González y Juliana Cárdenas.'
 
@@ -24,7 +21,7 @@ inicio_layout = html.Div(children=[
     html.Br(),
 
     #Titulo de la pagina
-    html.H1(children = '''Bienvenido al Programa de Predicción de Enfermedades Cardíacas''',
+    html.H1(children = '''Bienvenido al Tablero de Servicios de Salud de Colombia''',
             style={'textAlign': 'center'}),
     html.Br(),
 
@@ -32,7 +29,7 @@ inicio_layout = html.Div(children=[
 
     #Parrafo
     html.Div(children=[
-        html.Img(src='/assets/img1.png', style={'height': '20%', 'width': '20%','margin-right':'10px'}),
+        html.Img(src='/assets/salud-publica.png', style={'height': '20%', 'width': '20%','margin-right':'10px'}),
         html.Div(children=[
             html.Pre(texto, style={'text-align': 'center','padding':'1px'})
         ], style={'margin-left': '10px'})
@@ -46,11 +43,11 @@ inicio_layout = html.Div(children=[
     #Botones para ir de la pagina de inicio a las instrucciones y programa
     html.Div(children=[
         html.Div(children=[
-            dbc.Button("Instrucciones", size="lg", id="inicio_instrucciones", href="/instrucciones",
+            dbc.Button("Capacidad Instalada", size="lg", id="inicio_instrucciones", href="/instrucciones",
                        style={'margin-right': '10px', 'verticalAlign': 'middle'})],
             style={'display': 'inline-flex'}),
         html.Div(children=[
-            dbc.Button("Programa", size="lg", id="inicio_programa", href="/programa",
+            dbc.Button("Contratación", size="lg", id="inicio_programa", href="/programa",
                        style={'margin-left': '10px', 'verticalAlign': 'middle'})],
             style={'display': 'inline-flex'})],
         style={'margin-bottom': '10px',
